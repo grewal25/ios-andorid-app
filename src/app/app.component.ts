20f652083d9139a1f499d5fa41529633104bb8ff
Saranjeet Singh <grewalele@gmail.com>
2019-07-22 12:56:06 -0700

    added login functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,47 @@
 import { Component } from "@angular/core";
+import { User } from "./shared/user/user.model";
+import { UserService } from "./shared/user/user.service";
 
 @Component({
   selector: "gr-login",
+  providers: [UserService],
   moduleId: module.id,
-  templateUrl: "./login/login.component.html"
+  templateUrl: "./login/login.component.html",
+  styleUrls: ["./login/login.component.css"]
 })
-export class AppComponent {}
+export class AppComponent {
+    user: User;
+    isLoggingIn = true;
+  
+    constructor(private userService: UserService) {
+        this.user = new User();
+      }
+  
+      submit() {
+        if (this.isLoggingIn) {
+          this.login();
+        } else {
+          this.signUp();
+        }
+      }
+      
+      login() {
+        // TODO: Define
+      }
+      
+      signUp() {
+        this.userService.register(this.user)
+          .subscribe(
+            () => {
+              alert("Your account was successfully created.");
+              this.toggleDisplay();
+            },
+            () => alert("Unfortunately we were unable to create your account.")
+          );
+      }
+      
+  
+    toggleDisplay() {
+      this.isLoggingIn = !this.isLoggingIn;
+    }
+  }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from "@angular/core";
+import { NativeScriptFormsModule } from "nativescript-angular/forms";
+import { NativeScriptHttpModule } from "nativescript-angular/http";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
-
 
 import { AppComponent } from "./app.component";
 
 @NgModule({
   imports: [
     NativeScriptModule,
-
+    NativeScriptFormsModule,
+    NativeScriptHttpModule
   ],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
